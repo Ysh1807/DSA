4d639b50b87ce58a70ff1a777b8136f79c4e477e
Ysh1807 <120515487+Ysh1807@users.noreply.github.com>
2023-03-14 02:24:28 +0530

    Create JosephusProblem.py

---
+++ Recursion/JosephusProblem.py
@@ -0,0 +1,12 @@
+public class JosephusProblem {
+    public static void main(String[] args) {
+        System.out.println(jos(5,3));
+    }
+     public static int jos(int n, int k) {
+        if (n==1) {
+            return 0;
+        }
+
+        return ((jos(n-1,k)+k)%n);
+     }
+}
